d26bc30a3bdc7cbcb44096c983cf6751ac677a25
Elijah Insua <tmpvar@gmail.com>
2013-06-05 18:50:55 -0700

    unused edge.js

--- edge.js
+++
@@ -1,4 +0,0 @@
-module.exports = function(current, prev, delta, center) {
-  var centerPoint = (center) ? prev.add(current, true).divide(2) : current;
-  return centerPoint.subtract(prev, true).skew().normalize.multiply(-delta);
-};
